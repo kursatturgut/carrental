5c46aaeabea77d9ced01709a1fe975afb6a55e96
bzhunl <kursatturgut1352@gmail.com>
2022-03-04 09:06:50 -0800

    Initial commit

---
+++ src/main/java/com/lecture/carrental/domain/User.java
@@ -0,0 +1,109 @@
+package com.lecture.carrental.domain;
+
+import com.lecture.carrental.domain.enumeration.UserRole;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import java.util.HashSet;
+import java.util.Set;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="users")
+public class User {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Size(max = 15)
+    @NotNull(message = "please enter your first name")
+    @Column(nullable = false,length = 15)
+    private String firstName;
+
+    @Size(max = 15)
+    @NotNull(message = "please enter your last name")
+    @Column(nullable = false,length = 15)
+    private String lastName;
+
+    @Size(min = 4,max = 60)
+    @NotNull(message = "please enter your password")
+    @Column(nullable = false,length = 120)
+    private String password;
+
+    @Pattern(regexp = "^((\\(\\d{3}\\))|\\d{3})[- .]?\\d{3}[- .]?\\d{4}$",
+            message = "Please enter valid phone number")
+    @Size(min = 14,max = 14)
+    @NotNull(message = "please enter your phone number ")
+    @Column(nullable = false,length = 14)
+    private  String phoneNumber;
+
+
+    @Email(message = "please enter valid email")
+    @Size(min = 5,max = 150)
+    @Column(nullable = false,unique = true,length = 14)
+    @NotNull(message = "please enter mail")
+    private  String email;
+
+    @Size(max = 250)
+    @NotNull(message = "please enter your adres")
+    @Column(nullable = false,length = 250)
+    private  String address;
+
+
+    @Size(max = 15)
+    @NotNull(message = "please enter your zip code")
+    @Column(nullable = false,length = 15)
+    private  String zipCode;
+
+
+    @Column(nullable = false)
+    private Boolean builtIn=false;
+
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+
+
+    public Set<String> getRole() {
+        Set<String> roles1 = new HashSet<>();
+        Role[] role = roles.toArray(new Role[roles.size()]);
+        for (int i = 0; i < roles.size(); i++){
+            if (role[i].getName().equals(UserRole.ROLE_ADMIN))
+                roles1.add("Administrator");
+            else
+                roles1.add("Customer");
+        }
+        return roles1;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ src/main/java/com/lecture/carrental/repository/RoleRepository.java
@@ -0,0 +1,19 @@
+package com.lecture.carrental.repository;
+
+
+import com.lecture.carrental.domain.Role;
+import com.lecture.carrental.domain.enumeration.UserRole;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role,Integer> {
+
+// sorgu yaptık otomatik
+Optional<Role>findByName(UserRole name);
+
+
+
+}
